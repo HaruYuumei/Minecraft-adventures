ab81e154383944679d46b74e8aa7af80a6347b26
HaruYuumei <luzivotto.erick@gmail.com>
2025-03-26 17:53:37 -0300

    First commit

---
+++ src/main/java/haru/runeore/RuneOreCraft.java
@@ -0,0 +1,19 @@
+package haru.runeore;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RuneOreCraft implements ModInitializer {
+	public static final String MOD_ID = "runeorecraft";
+
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/haru/runeore/RuneOreCraftClient.java
@@ -0,0 +1,10 @@
+package haru.runeore;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RuneOreCraftClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
